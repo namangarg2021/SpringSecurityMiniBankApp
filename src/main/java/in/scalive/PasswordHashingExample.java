47c93900a51d749a39f3bab648feeac059c04bf3
Naman Garg <naman.garg@dltapps.co.uk>
2025-06-05 17:49:39 +0530

    PasswordHashingExample code.

--- src/main/java/in/scalive/PasswordHashingExample.java
+++ src/main/java/in/scalive/PasswordHashingExample.java
@@ -3,20 +3,18 @@ package in.scalive;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-public class Test {
+public class PasswordHashingExample {
 	public static void main(String[] args) {
 		//Simulating Registration
-		String originalPwd = "sachin";
+		String originalPwd = "naman";
 		PasswordEncoder pEncoder = new BCryptPasswordEncoder(24);
 		String encodedPwd = pEncoder.encode(originalPwd);
 		System.out.println("original:" + originalPwd);
 		System.out.println("Hashed pwd:" + encodedPwd);
 		
 		//Simulate login
-		originalPwd = "sachin";
+		originalPwd = "naman";
 		boolean result = pEncoder.matches(originalPwd, encodedPwd);
 		System.out.println("Res:" + result);
-		
-		
 	}
 }
